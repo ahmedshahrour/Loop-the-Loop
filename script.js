0b539d18855551605fa18731b513ce989c0d9ece
AhmedShahrour <>
2021-05-30 12:51:23 +0000

    added files

---
+++ script.js
@@ -0,0 +1,73 @@
+//var catnumber = ['Abd Erazek', 'Adham', 'Afnan', 'Hiba', 12, true, ['code101', 'code102', 'code201']];
+
+// console.log(studentsName);
+// console.log(studentsName[1]);
+// console.log(studentsName[3]);
+
+// console.log(studentsName[0], ',' ,studentsName[2])
+
+// console.log(studentsName.length)
+// Start point
+// Comparing between the start point and end point (Count oof the value inside the array)
+// i = i + 1 (increase the i var)
+
+var userName = prompt("Whats your name ?");
+alert ("welcome to our websit " + userName) 
+
+var c = prompt('do you like cats ?')
+
+ while (c!="yes" || c!="No" )
+ {if (c=='yes') 
+ {alert ('thanks'); break;}
+ }
+ 
+ var catnumber = prompt('How many cats do you want?');
+
+for(var i = 0; i < catnumber; i++) {
+  
+  // document.write(studentsName[i]);
+  // if(studentsName[i] == 'Hiba') {
+  //   break;
+  // }
+  // console.log(studentsName[i]);
+  
+  document.write('<img src="https://www.mcicon.com/wp-content/uploads/2021/03/Cat-03.jpg" />')
+}
+
+
+// logical 
+// && and &
+// || or
+
+// var username = prompt('Please enter your name');
+
+// var userAge = prompt('please enter age');
+
+// if(username == 'Mamoon' && userAge == 23) { 
+//   console.log(true); 
+// }
+
+// if(username == 'Dania' || userAge == 25) { 
+//   console.log(true); 
+// }
+
+// if(username == 'Mamoon' && userAge != 23) { 
+//   console.log(true); 
+// }
+
+//if the user age equal to 30 loop again
+// while(userAge == 30) {
+// console.log('inside the while loop');
+// break;
+// userAge = prompt('please enter your age again!!!!');
+// }
+
+
+
+// while(username != 'Renad' || userAge != 22) {
+// console.log('inside the whie loop');
+// break;
+// username = prompt('Please enter your name');
+// userAge = prompt('please enter age');
+
+// }
